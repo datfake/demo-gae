f94c5582026a93a651d4bece956a84f02d0d9a0b
DatNgo <dat.ngo@bnksolution.com>
2022-08-10 17:03:49 +0700

    init

---
+++ src/test/java/com/datfngo/demogae/DemogaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datfngo.demogae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemogaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
